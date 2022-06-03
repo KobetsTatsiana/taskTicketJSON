34edc7fc174cc32439304e9dace25fca8e1e3a2a
Tatsiana Kobets <tatsiana.kobets@yandex.ru>
2022-06-03 14:29:40 +0300

    Checking and fixing project

--- src/main/java/by/htp/ticket/main/Main.java
+++ src/main/java/by/htp/ticket/main/Main.java
@@ -13,24 +13,23 @@ public class Main {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
             TicketList ticketList;
             while (true) {
-                System.out.println("Введите путь к файлу:");
+                System.out.println("Введи путь к файлу tickets.json:"); //src/main/resources/tickets.json
                 try {
                     String path = reader.readLine();
-                    //path = "src/main/resources/tickets.json";
                     ticketList = ReadingTicketJson
                             .parse(path);
                     break;
                 } catch (Exception e) {
-                    System.out.println("Неверный путь к файлу.");
+                    System.out.println("Неверный путь. Попробуй еще раз)");
                 }
             }
 
-            System.out.println("Среднее время полета между городами Владивосток и Тель-Авив : "
+            System.out.println("1. Среднее время полета между городами Владивосток и Тель-Авив: "
                     + DataAnalysis.getAvg(ticketList,
                     "Владивосток",
                     "Тель-Авив"));
 
-            System.out.println("90-й процентиль времени полета между городами  Владивосток и Тель-Авив : "
+            System.out.println("2. 90-й процентиль времени полета между городами Владивосток и Тель-Авив: "
                     + DataAnalysis.getPercentile(ticketList,
                     90,
                     "Владивосток",
